0c150d5d12cb9cc717f0ee6bdc8576ae54f44a7c
CrazyMKing <CrazyMKing@CrazyMKing-2.local>
2019-04-02 15:43:37 -0400

    s

--- code.js
+++ code.js
@@ -6,14 +6,16 @@ var draw=function(data)
   var margins = {top: 20, right: 10, bottom: 40, left: 70}
   var height=screen.height-margins.top-margins.bottom
   var width=screen.width-margins.right-margins.left
+  var legengdata=["puppies","kittens"]
   var pound=data.puppies.map(function(d,i){
     return {
       puppies:data.puppies[i],
       kittens:data.kittens[i],
-      day:i,
-      pets:data.puppies[i]+data.kittens[i]
+      // day:i,
+      // pets:data.puppies[i]+data.kittens[i]
     }
   })
+  var color=d3.scaleOrdinal(d3.schemeSet3)
   var xScale=d3.scaleLinear()
      .domain([0,pound.length])
      .nice()
@@ -27,6 +29,7 @@ var draw=function(data)
             .attr("id","graph")
             .attr("width",screen.width)
             .attr("height",screen.height)
+  //line and area
   var line = svg.append('g')
                 .attr('transform', 'translate(' + margins.left + ',' + margins.top+ ')')
   var area = svg.append('g')
@@ -38,10 +41,38 @@ var draw=function(data)
       .datum(pound)
       .attr("d",drawLine)
       .attr("fill","none")
-      .attr("stroke", "steelblue")
+      .attr("stroke", color("puppies"))
+  //axis
   var xAxis=d3.axisBottom(xScale)
+              .ticks(pound.length)
   var yAxis=d3.axisLeft(yScale)
               .ticks(d3.max(pound,function(d){
                 return d3.max([d.puppies,d.kittens])}))
-  
+  svg.append("g")
+     .attr("id","xAxis")
+     .call(xAxis)
+     .attr('transform', 'translate(' + (margins.left)+ ',' + (height+margins.top) + ')')
+  svg.append("g")
+     .attr("id","yAxis")
+     .call(yAxis)
+     .attr('transform', 'translate(' + 65 + ',' + margins.top + ')')
+  //legend
+  var legend=svg.append("g")
+                .classed("legend",true)
+                .attr("transform","translate("+(width+margins.left)+","+margins.top+")");
+  var legendLines=legend.selectAll("g")
+                        .data(legengdata)
+                        .enter()
+                        .append("g")
+                        .classed("legendLine",true)
+                        .attr("transform",function(d,i){
+                        return "translate(0,"+(i*20)+")";});
+  // ;
+  // legendLines.append("rect")
+  //            .attr("x",0)
+  //            .attr("y",0)
+  //            .attr("width",10)
+  //            .attr("height",10)
+  //            .attr("fill",function(d){return colors(d.name);});
+
 }
